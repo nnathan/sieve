cf3b14cfde046e98dc934d7eb9d63b917ad93c62
Naveen Nathan <naveen@lastninja.net>
2021-10-02 09:50:38 +1000

    lexer: increase test coverage

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -20,15 +20,20 @@ func TestLexer(t *testing.T) {
 		{"#\r", `1:3 <illegal> "expected \\n after \\r"`},
 		{" \t#foo\xff\n\r\n", ""},
 
-		// identifiers and numbers
+		// identifiers, tags, and numbers
 		{"foo", `1:1 identifier "foo"`},
+		{"_foo", `1:1 identifier "_foo"`},
 		{"fo\xff", `1:1 identifier "fo", 1:3 <illegal> "\xff"`},
+		{":foo", `1:1 tag ":foo"`},
+		{":0", `1:2 <illegal> "expected identifier character ([a-zA-Z_])"`},
 		{"100", `1:1 number "100"`},
 		{"100\xff", `1:1 number "100", 1:4 <illegal> "\xff"`},
 		{"100m", `1:1 number "100m"`},
 
 		// multline strings
+		{"text:.\n", `1:1 multline ""`},
 		{"text:\n.\n", `1:1 multline ""`},
+		{"text:foo\n.\n", `1:1 multline "foo\n"`},
 		{"text: \t#foo\x00\n.\n", `1:1 multline ""`},
 		{"text:\r\nfoo\nbar\r\n.\n", `1:1 multline "foo\nbar\r\n"`},
 		{"text:", `1:6 <illegal> "premature EOF trying to read multiline string"`},
@@ -36,11 +41,15 @@ func TestLexer(t *testing.T) {
 		{"text:#\r", `1:8 <illegal> "expected \\n after \\r"`},
 		{"text:#", `1:7 <illegal> "premature EOF trying to read multiline string"`},
 		{"text:\n..\n...\n.foo\n..foo\n...foo\n.\n", `1:1 multline ".\n..\n.foo\n.foo\n..foo\n"`},
+		{"text:\nfoo\r.\n", `2:5 <illegal> "expected \\n after \\r"`},
+		{"text:\nfoo\x00\n.\n", `2:4 <illegal> "invalid NUL character encountered in multiline string"`},
+		{"text:\nfoo", `2:4 <illegal> "premature EOF trying to read multiline string"`},
 
 		// quoted strings
 		{`""`, `1:1 string ""`},
 		{"\"x\xff\"", `1:1 string "x\xff"`},
 		{"\"x\r\n\"", `1:1 string "x\r\n"`},
+		{"\"\\\"\"", `1:1 string "\""`},
 		{"\"x\r\"", `1:4 <illegal> "expected \\n after \\r"`},
 		{"\"\x00", `1:2 <illegal> "invalid NUL character encountered in string"`},
 		{"\"", `1:2 <illegal> "premature EOF trying to read string"`},
@@ -48,11 +57,16 @@ func TestLexer(t *testing.T) {
 		// bracketed comments
 		{"/**/", ""},
 		{"/*foo\r\nbar\nbaz*/", ""},
+		{"/*foo*bar*baz*/", ""},
 		{"/* /* **/", ""},
 		{"/*foo*/ /*baz*/", ""},
 		{"/*foo\rbar*/", `1:7 <illegal> "expected \\n after \\r"`},
 		{"/*foo\x00\xff*/", `1:6 <illegal> "invalid NUL character encountered in bracketed comment"`},
+		{"/x", `1:2 <illegal> "expecting '*' to begin bracketed comment"`},
 		{"/* */ */", `1:7 <illegal> "*"`},
+		{"/**", `1:4 <illegal> "premature EOF trying to read bracketed comment"`},
+		{"/**\x00*/", `1:4 <illegal> "invalid NUL character encountered in bracketed comment"`},
+		{"/**\r*/", `1:5 <illegal> "expected \\n after \\r"`},
 
 		// invalid characters
 		{"%", `1:1 <illegal> "%"`},
